8b303dca4448de299ace6abc4422511cc10afbaa
Cam C <ckc.cameron@gmail.com>
2020-10-29 18:08:43 -0700

    Add update_or_create class method to Student model
    * finds Student by their unique id or creates a new Student
    * sets attributes for student when they are created
    
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>
    Co-authored-by: Judith Pillado <602436900+judithpillado@users.noreply.github.com>
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Corey Carter <59756152+coreycartercodes@users.noreply.github.com>
    Co-authored-by: Garrett Gerdsen <45319985+ggerdsen@users.noreply.github.com>

--- app/models/student.rb
+++ app/models/student.rb
@@ -11,4 +11,22 @@ class Student < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :token, presence: true
   validates :refresh_token, presence: true
+
+  def self.update_or_create(auth)
+    student = Student.find_by(uid: auth[:uid]) || Student.new
+
+    student.attributes = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      email: auth[:info][:email],
+      first_name: auth[:info][:first_name],
+      last_name: auth[:info][:last_name],
+      token: auth[:credentials][:token],
+      refresh_token: auth[:credentials][:refresh_token]
+      }
+
+    student.save
+
+    student
+  end
 end
