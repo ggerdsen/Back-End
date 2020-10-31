547a3d4bd01c2c8e0d7f8f90aea1ee41e9b5311b
Philip DeFraties <philipjfry338@gmail.coom>
2020-10-31 16:10:45 -0600

    Adds edit, index, and show actions for teachers/courses_controller
    Co-authored-by: Priya Power 
    <49959312+priyapower@users.noreply.github.com>
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    Co-authored-by: Chris Allbritton 
    <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Garrett Gerdsen 
    <45319985+ggerdsen@users.noreply.github.com>
    Co-authored-by: Daniel Halverson 
    <63027888+dhalverson@users.noreply.github.com>

--- app/controllers/api/v1/teachers/courses_controller.rb
+++ app/controllers/api/v1/teachers/courses_controller.rb
@@ -5,12 +5,18 @@ module Api
     module Teachers
       class CoursesController < ApplicationController
 
-        def update
-          render json: TeacherCourseSerializer.new(Course.update(params[:id], course_params))
-          #
+        def edit
+          render json: TeacherCourseSerializer.new(Course.update(params[:id]))
+          binding.pry
         end
 
+        def index
+          render json: TeacherCourseSerializer.new(Course.all)
+        end
 
+        def show
+          render json: TeacherCourseSerializer.new(Course.find(params[:id]))
+        end
         private
 
         def course_params
