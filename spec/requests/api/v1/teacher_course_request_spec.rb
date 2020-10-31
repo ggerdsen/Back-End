e30485d5ba534df1f45692705bfa48bafc7f6f12
Saryn Mooney <smooney15@my.whitworth.edu>
2020-10-30 22:14:03 -0600

    teacher course request spec still a work in progress

---
+++ spec/requests/api/v1/teacher_course_request_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'teachers courses' do
+  it 'updates a course' do
+    teacher = create(:teacher)
+
+    course = create(:course, teacher_id: teacher.id)
+
+    patch "/api/v1/teachers/courses/#{course.id}"
+
+    expect(response).to be_successful
+
+    course = JSON.parse(response.body, symbolize_names: true)
+    # expect(response).to be_successful
+    #
+    # expect(students).to have_key(:data)
+    # expect(students[:data]).to be_an(Array)
+    # expect(students[:data].count).to eq(3)
+
+
+  end
+end
