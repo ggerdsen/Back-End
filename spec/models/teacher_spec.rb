38c3521984f043d3961ebc578b52a934e34b0955
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-10-31 17:28:57 -0500

    SUBJECT: Adds validations to teacher regarding oauth information.
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -4,6 +4,16 @@ require 'rails_helper'
 
 RSpec.describe Teacher do
   describe 'Validations' do
-    it { should validate_presence_of :name }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :provider }
+    it { should validate_presence_of :uid }
+    it { should validate_uniqueness_of :uid }
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_presence_of :token }
+    it { should validate_presence_of :refresh_token }
+    it { should validate_presence_of :school_name }
+    it { should validate_presence_of :school_district }
   end
 end
