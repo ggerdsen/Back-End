e60c48b7e50795de0941780ed5d0fd1348b79a37
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-10-31 15:46:26 -0500

    SUBJECT: Add update_or_create model method to teacher model.
    DETAILS: Need to Knows AND Nice to Knows
    
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -4,4 +4,20 @@ class Teacher < ApplicationRecord
   has_many :courses
 
   validates_presence_of :name
+    def self.update_or_create(auth)
+      teacher = Teacher.find_by(uid: auth[:uid]) || Teacher.new
+      teacher.attributes = {
+        provider: auth[:provider],
+        uid: auth[:uid],
+        email: auth[:email],
+        first_name: auth[:first_name],
+        last_name: auth[:last_name],
+        token: auth[:token],
+        refresh_token: auth[:refresh_token]
+        }
+
+      teacher.save
+
+      teacher
+    end
 end
