3fa39a17de2bd02d6096229b4f5c75a6cafac9e7
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-10-31 16:31:23 -0500

    Work in progress for authorization.
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  protect_from_forgery with: :exception
+
+  def current_user
+    require "pry"; binding.pry
+    @current_user
+
+  end
 end
