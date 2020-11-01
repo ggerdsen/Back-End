8b4b93eba029996ed6579067758e02c55f3ba41d
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-10-31 19:58:07 -0500

    SUBJECT: delete's current_user method that was decided was better off in the front end.
    Co-authored-by: Cam C <57038617+ckccameron@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :exception
 
-  def current_user
-    require "pry"; binding.pry
-    @current_user
-
-  end
 end
