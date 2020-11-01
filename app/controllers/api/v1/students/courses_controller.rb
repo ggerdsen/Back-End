5b26050525966dbe8101e329abf9c89efbab57d8
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-01 11:11:27 -0700

    Updated points to student_points
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>

--- app/controllers/api/v1/students/courses_controller.rb
+++ app/controllers/api/v1/students/courses_controller.rb
@@ -2,14 +2,14 @@ module Api
   module V1
     module Students
       class CoursesController < ApplicationController
-        
+
         def create
-          render json: CourseStudentSerializer.new(CourseStudent.create(points: course_params[:points], course_id: course_params[:course_id], student_id: course_params[:student_id]))
+          render json: CourseStudentSerializer.new(CourseStudent.create(student_points: course_params[:student_points], course_id: course_params[:course_id], student_id: course_params[:student_id]))
         end
 
         private
         def course_params
-          params.permit(:points, :course_id, :student_id)
+          params.permit(:student_points, :course_id, :student_id)
         end
       end
     end
