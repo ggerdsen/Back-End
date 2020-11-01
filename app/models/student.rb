9329a2c2ac52e2f54c24d99939cc92162c3178fe
Cam C <ckc.cameron@gmail.com>
2020-11-01 12:55:39 -0800

    Adjust hash parsing to reflect the actual auth hash
    * actual hash in OAuth handshake looks different than what we had so we
    changed how we parse the auth to access the attirbute values we need for
    backend database
    
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,18 +10,17 @@ class Student < ApplicationRecord
   validates :uid, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :token, presence: true
-  validates :refresh_token, presence: true
+  # validates :refresh_token, presence: true
 
   def self.update_or_create(auth)
     student = Student.find_by(uid: auth[:uid]) || Student.new
     student.attributes = {
-      provider: auth[:provider],
-      uid: auth[:uid],
-      email: auth[:email],
-      first_name: auth[:first_name],
-      last_name: auth[:last_name],
-      token: auth[:token],
-      refresh_token: auth[:refresh_token]
+      provider: auth["access_token"]["provider"],
+      uid: auth["access_token"]["uid"],
+      email: auth["access_token"]["extra"]["raw_info"]["email"],
+      first_name: auth["access_token"]["extra"]["raw_info"]["given_name"],
+      last_name: auth["access_token"]["extra"]["raw_info"]["family_name"],
+      token: auth["access_token"]["credentials"]["token"]
       }
 
     student.save
