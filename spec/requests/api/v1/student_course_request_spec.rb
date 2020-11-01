4042859cf6eba2f553496c46ec4685c1c3418770
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-01 14:36:07 -0700

    Update student course spec for robust testing
    - Calls ActiveRecord and has expectations for course id
    - Checks api response and json parse for course id
    
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>

--- spec/requests/api/v1/student_course_request_spec.rb
+++ spec/requests/api/v1/student_course_request_spec.rb
@@ -12,19 +12,20 @@ RSpec.describe 'student courses' do
       teacher_id: teacher.id,
       course_points: 0
     })
-
     student = create(:student)
+
     student_course_params = ({
       course_id: course.id,
       student_id: student.id,
       student_points: 0
     })
-
     post "/api/v1/students/courses", params: student_course_params
-
+    
     expect(response).to be_successful
+    returned_student_course = JSON.parse(response.body, symbolize_names: true)
+    expect(returned_student_course[:data][:id].to_i).to eq(course.id)
 
-    returned_course = JSON.parse(response.body, symbolize_names: true)
-    require "pry"; binding.pry
+    student_course_in_database = CourseStudent.last
+    expect(student_course_in_database.course_id).to eq(course.id)
   end
 end
