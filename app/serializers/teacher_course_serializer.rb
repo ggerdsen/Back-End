f191652201d18dd0c512e9fbd592de8040dbbbf6
Philip DeFraties <philipjfry338@gmail.coom>
2020-11-01 15:22:40 -0700

    Adds teacher association and students attribute (for pulling student names rather than just ids) to TeacherCourseSerializer

--- app/serializers/teacher_course_serializer.rb
+++ app/serializers/teacher_course_serializer.rb
@@ -1,4 +1,9 @@
 class TeacherCourseSerializer < BaseSerializer
   attributes :name, :course_code, :school_name
+  belongs_to :teacher
   has_many :students, through: :course_students
+  
+  attribute :students do |course|
+    StudentSerializer.new(course.students)
+  end
 end
