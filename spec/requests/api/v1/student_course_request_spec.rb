8212da31f536f397a022c416d3617df34bd41e91
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-01 11:22:08 -0700

    Request spec in progress for student_course
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>

--- spec/requests/api/v1/student_course_request_spec.rb
+++ spec/requests/api/v1/student_course_request_spec.rb
@@ -2,24 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe 'student courses' do
-  xit 'updates a course' do
-    student = create(:teacher)
-
-    course = create(:course, teacher_id: teacher.id)
-
-    patch "/api/v1/teachers/courses/#{course.id}"
-
-    expect(response).to be_successful
-
-    course = JSON.parse(response.body, symbolize_names: true)
-    # expect(response).to be_successful
-    #
-    # expect(students).to have_key(:data)
-    # expect(students[:data]).to be_an(Array)
-    # expect(students[:data].count).to eq(3)
-
-
-  end
 
   it 'creates a new student course' do
     teacher = create(:teacher)
@@ -27,22 +9,22 @@ RSpec.describe 'student courses' do
       name: 'Principles of Real Estate',
       course_code: 'abcd1234',
       school_name: 'Hogwarts High School',
-      teacher_id: teacher.id
-    })
-    
-    student = Student.create!(name: Faker::Name.name )
-    course_params = ({
       teacher_id: teacher.id,
-      course_id: course.id,
-      student_id: student.id,
-      points: 0
+      course_points: 0
     })
 
-    post "/api/v1/students/courses", params: course_params
-    
+    student = create(:student)
+    student_course_params = ({
+      course_id: course.id,
+      student_id: student.id,
+      student_points: 0
+    })
+
+    post "/api/v1/students/courses", params: student_course_params
+
     expect(response).to be_successful
 
     course = JSON.parse(response.body, symbolize_names: true)
-  
+    require "pry"; binding.pry
   end
 end
