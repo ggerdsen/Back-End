a17cc07e52c9691fed9ad58ca8a2b452c5c52df6
Cam C <ckc.cameron@gmail.com>
2020-11-02 13:58:33 -0800

    Add request test for find route for students
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- spec/requests/api/v1/students_request_spec.rb
+++ spec/requests/api/v1/students_request_spec.rb
@@ -63,6 +63,43 @@ describe 'Students API' do
     expect(student[:relationships][:courses][:data][0]).to have_key(:type)
   end
 
+  it "can find a teacher by uid" do
+    student1 = create(:student, uid: "100")
+    student2 = create(:student, uid: "101")
+    student3 = create(:student, uid: "102")
+
+    get "/api/v1/students/find/#{student1.uid}"
+
+    expect(response).to be_successful
+    student = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(student[:attributes][:uid]).to eq(student1.uid)
+
+    expect(student).to have_key(:id)
+    expect(student[:id]).to be_an(String)
+
+    expect(student).to have_key(:attributes)
+    expect(student[:attributes]).to be_a(Hash)
+
+    expect(student[:attributes]).to have_key(:first_name)
+    expect(student[:attributes][:first_name]).to be_a(String)
+
+    expect(student[:attributes]).to have_key(:last_name)
+    expect(student[:attributes][:last_name]).to be_a(String)
+
+    expect(student[:attributes]).to have_key(:uid)
+    expect(student[:attributes][:uid]).to be_a(String)
+
+    expect(student[:attributes]).to have_key(:email)
+    expect(student[:attributes][:email]).to be_a(String)
+
+    expect(student[:attributes]).to have_key(:token)
+    expect(student[:attributes][:token]).to be_a(String)
+
+    expect(student[:attributes]).to have_key(:refresh_token)
+    expect(student[:attributes][:refresh_token]).to be_a(String)
+  end
+
   it 'can get one student by its id' do
     id = create(:student).id
 
