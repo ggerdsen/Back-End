d0e9b26623b300f15e1fb6c06a717a31d99a5570
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-01 21:41:06 -0700

    Update index action
    - Attempting to solve sql joins here
    - Needs to return course data: name, teacher id, course points, school 
    name and course code
    - Needs to return course student data of student id, student points
    - Needs to group by course.id and course_students.course_id

--- app/controllers/api/v1/students/courses_controller.rb
+++ app/controllers/api/v1/students/courses_controller.rb
@@ -2,15 +2,18 @@ module Api
   module V1
     module Students
       class CoursesController < ApplicationController
+        def index
+          # courses = Course.joins(:course_students).select("courses.*, course_students.*").where("course_students.student_id = #{course_params[:student_id]}")
+          course_student_ids = CourseStudent.where(student_id: course_params[:student_id]).pluck(:course_id)
+          Course.select("course.*, course_students.student_points").joins(:course_students).where(id: course_student_ids)
+          binding.pry
+          courses = Course.where(id: course_student_ids)
+          render json: CourseSerializer.new(courses)
+        end
 
         def create
           cs = CourseStudent.create(student_points: course_params[:student_points], course_id: course_params[:course_id], student_id: course_params[:student_id])
           render json: CourseStudentSerializer.new(cs)
-        end
-
-        def index
-          #course_students = CourseStudent.where(course_params[:student_id])
-          render json: CourseStudentSerializer.new(CourseStudent.where(student_id: course_params[:student_id]))
         end
 
         private
