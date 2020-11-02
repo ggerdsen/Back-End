b0c2c79af87bb67292bc6d0b1e517b688be0a9a9
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-01 17:30:58 -0700

    Add teacher::search controller
    - Action show
    - Find course by course code and renders through teacher course serializer
    
    Co-authored-by: Saryn Mooney
    <smooney15@my.whitworth.edu>

---
+++ app/controllers/api/v1/teachers/search_controller.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    module Teachers
+      class SearchController < ApplicationController
+        def show
+          exist = Course.find_by(course_code: course_params[:courseCode])
+          render json: TeacherCourseSerializer.new(exist)
+        end
+
+        private
+
+        def course_params
+          params.permit(:courseCode)
+        end
+      end
+    end
+  end
+end
