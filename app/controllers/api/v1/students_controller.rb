9c0280e3ca3b431dd9a4c89cef4a9660bb251ef4
Cam C <ckc.cameron@gmail.com>
2020-11-03 11:45:52 -0800

    Fix StudentsController create action to access student_params with info needed to successfully create student user
    * got rid of strong params for student_params and moved within create
    action to parse the StringIO that was showing up
    
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>

--- app/controllers/api/v1/students_controller.rb
+++ app/controllers/api/v1/students_controller.rb
@@ -16,23 +16,19 @@ module Api
       end
 
       def create
-        Student.update_or_create(student_params)
-        render json: StudentSerializer.new(Student.last)
+        student_params = JSON.parse(request.body.read, symbolize_names: true)
+        student = Student.update_or_create(student_params)
+        render json: StudentSerializer.new(student)
       end
 
       def update
+        student_params = JSON.parse(request.body.read, symbolize_names: true)
         render json: StudentSerializer.new(Student.update(params[:id], student_params))
       end
 
       def destroy
         Student.destroy(params[:id])
         render body: nil, status: :no_content
-      end
-
-      private
-
-      def student_params
-        params.permit(:first_name, :last_name, :provider, :uid, :email, :token, :refresh_token)
       end
     end
   end
