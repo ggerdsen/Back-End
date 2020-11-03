a5529526e7d2eba077d701fbc4ccd5167feabcd9
Chris Allbritton <cla.inbox@gmail.com>
2020-11-03 13:27:59 -0500

    Refactor course model test to remove course_code validation
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu> 
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 RSpec.describe Course do
   describe 'Validations' do
     it { should validate_presence_of :name }
-    it { should validate_presence_of :course_code }
     it { should validate_presence_of :school_name }
     it { should validate_presence_of :course_points}
   end
