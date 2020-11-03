db205a895d571d932c34f5560635fff43b3d2857
Philip DeFraties <philipjfry338@gmail.coom>
2020-11-03 13:10:19 -0700

    Edits faraday call in trivia_q endpoint
    Co-authored-by: Garrett Gerdsen 
    <45319985+ggerdsen@users.noreply.github.com>
    Co-authored-by: Daniel Halverson 
    <63027888+dhalverson@users.noreply.github.com>
    Co-authored-by: Tyler Fields 
    <64871701+fieldstyler@users.noreply.github.com>

--- app/controllers/api/v1/poms_controller.rb
+++ app/controllers/api/v1/poms_controller.rb
@@ -3,7 +3,9 @@ module Api
     class PomsController < ApplicationController
 
       def single_trivia_question
-        response = Faraday.get("http://localhost:4567/api/v1/single_trivia_question")
+        response = Faraday.get("http://localhost:3001/api/v1/single_trivia_question")
+        binding.pry
+        body = JSON.parse(response.body, symbolize_names: true)
       end
     end
   end
