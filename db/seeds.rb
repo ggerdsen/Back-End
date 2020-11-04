a241602031ed8653d9b68a47f2aaa73a2d127d76
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-03 17:24:57 -0700

    Update seeds for wars

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,7 @@ class Seed
     seed = Seed.new
     seed.seed_students_teachers_courses
     seed.no_connect_courses
+    seed.seed_wars
     ActiveRecord::Base.connection.tables.each do |t|
       ActiveRecord::Base.connection.reset_pk_sequence!(t)
     end
@@ -64,7 +65,17 @@ class Seed
     #sad path seeds (teachers with courses and no connections to students)
   def no_connect_courses
     2.times do |i|
-      teacher = Teacher.create!(name: Faker::Name.name)
+      teacher = Teacher.create!({
+        provider: "Google",
+        uid: Faker::Number.number(digits: 5),
+        email: Faker::Internet.email,
+        token: Faker::Alphanumeric.alphanumeric(number: 12),
+        refresh_token: Faker::Alphanumeric.alphanumeric(number: 12),
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        school_name: Faker::Educator.university,
+        school_district: Faker::Address.city
+      })
       2.times do |i|
         course = teacher.courses.create!(
           name: Faker::Educator.subject,
@@ -73,6 +84,22 @@ class Seed
           course_points: Faker::Number.within(range: 0..100000)
         )
       end
+    end
+  end
+
+  def seed_wars
+    3.times do |i|
+      challenger = Teacher.find(Teacher.ids.sample)
+      opponent = Teacher.find(Teacher.ids.sample)
+      until challenger != opponent
+        opponent = Teacher.find(Teacher.ids.sample)
+      end
+      challenger.wars.create!({
+        challenger_course_id: challenger.id,
+        opponent_course_id: opponent.id,
+        challenger_course_points: challenger.courses.first.course_points,
+        opponent_course_points: opponent.courses.first.course_points
+        })
     end
   end
 end
