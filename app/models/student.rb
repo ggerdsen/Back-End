6edfe23d10655d0ec162df41d2ec835813843b5c
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-04 11:50:31 -0700

    Add model methods for oauth handshake for create
    Co-authored-by: Saryn Mooney <smooney15@my.whitworth.edu>
    Co-authored-by: Chris Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Arique1104 <60521756+Arique1104@users.noreply.github.com>
    Co-authored-by: Corey Carter <59756152+coreycartercodes@users.noreply.github.com>

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,4 +10,22 @@ class Student < ApplicationRecord
   validates :uid, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :token, presence: true
+  validates :role, presence: true
+
+  def self.update_or_create(auth)
+    student = Student.find_by(uid: auth[:uid]) || Student.new
+    student.attributes = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      email: auth[:email],
+      first_name: auth[:first_name],
+      last_name: auth[:last_name],
+      token: auth[:token],
+      role: "student"
+      }
+
+    student.save
+
+    student
+  end
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -10,6 +10,23 @@ class Teacher < ApplicationRecord
   validates :uid, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :token, presence: true
-  # validates :school_name, presence: true
-  # validates :school_district, presence: true
+  validates :role, presence: true
+
+  def self.update_or_create(auth)
+    teacher = Teacher.find_by(uid: auth[:uid]) || Teacher.new
+    teacher.attributes = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      email: auth[:email],
+      first_name: auth[:first_name],
+      last_name: auth[:last_name],
+      token: auth[:token],
+      school_name: auth[:school_name],
+      school_district: auth[:school_district],
+      role: "teacher"
+      }
+
+    teacher.save
+    teacher
+  end
 end
