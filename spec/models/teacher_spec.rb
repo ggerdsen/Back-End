65688c116434722eb5a4d1a73c443d52c74dc7a1
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-03 17:24:57 -0700

    Update typo on teacher spec
    - Changed Student to Teacher

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.describe Student do
+RSpec.describe Teacher do
   describe 'Validations' do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
