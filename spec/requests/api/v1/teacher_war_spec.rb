fece30074804dc57bac78b0aeeceff4a86fa19f0
Priya Power <49959312+priyapower@users.noreply.github.com>
2020-11-05 11:06:44 -0700

    Add spec for all wars visible by teacher

--- spec/requests/api/v1/teacher_war_spec.rb
+++ spec/requests/api/v1/teacher_war_spec.rb
@@ -32,8 +32,8 @@ RSpec.describe 'teachers wars' do
     })
     war_params = ({
       teacher_id: challenger.id,
-      challenger_course_id: challenger.id,
-      opponent_course_id: opponent.id,
+      challenger_course_id: course1.id,
+      opponent_course_id: course2.id,
       challenger_course_points: course1.course_points,
       opponent_course_points: course2.course_points
     })
@@ -62,16 +62,70 @@ RSpec.describe 'teachers wars' do
       course_points: 2500
     })
     war = challenger.wars.create({
-      challenger_course_id: challenger.id,
-      opponent_course_id: opponent.id,
+      challenger_course_id: course1.id,
+      opponent_course_id: course2.id,
       challenger_course_points: course1.course_points,
       opponent_course_points: course2.course_points
     })
     expect(War.count).to eq(1)
     delete "/api/v1/teachers/courses/wars/#{war.id}"
     expect(response).to be_successful
-    expect(response).to be_successful
     expect(War.count).to eq(0)
     expect{War.find(war.id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+
+  it 'can see all wars associated with a teacher' do
+    challenger = create(:teacher)
+    course1 = challenger.courses.create({
+      name: 'Principles of Real Estate',
+      course_code: 'abcd1234',
+      school_name: 'Hogwarts High School',
+      course_points: 2000
+    })
+    course2 = challenger.courses.create({
+      name: 'Principles of Real Estate',
+      course_code: 'abcd1234',
+      school_name: 'Hogwarts High School',
+      course_points: 2000
+    })
+
+    opponent1 = create(:teacher)
+    course3 = opponent1.courses.create({
+      name: 'Forensic Psychology',
+      course_code: '1234abcd',
+      school_name: 'Hogwarts High School',
+      course_points: 2500
+    })
+    opponent2 = create(:teacher)
+    course4 = opponent2.courses.create({
+      name: 'Forensic Psychology',
+      course_code: '1234abcd',
+      school_name: 'Hogwarts High School',
+      course_points: 2500
+    })
+    war1 = challenger.wars.create({
+      challenger_course_id: course1.id,
+      opponent_course_id: course3.id,
+      challenger_course_points: course1.course_points,
+      opponent_course_points: course3.course_points
+    })
+    war2 = challenger.wars.create({
+      challenger_course_id: course2.id,
+      opponent_course_id: course4.id,
+      challenger_course_points: course2.course_points,
+      opponent_course_points: course4.course_points
+    })
+    expect(War.count).to eq(2)
+    teacher_wars_params = ({
+      teacher_id: challenger.id
+      })
+    get "/api/v1/teachers/courses/wars", params: teacher_wars_params
+    expect(response).to be_successful
+    all_wars = JSON.parse(response.body, symbolize_names: true)[:data]
+    expect(all_wars.count).to eq(2)
+    expect(all_wars[0][:id].to_i).to eq(war1.id)
+    expect(all_wars[0][:attributes][:challenger_course_points]).to eq(war1.challenger_course_points)
+    expect(all_wars[1][:id].to_i).to eq(war2.id)
+    expect(all_wars[1][:attributes][:challenger_course_points]).to eq(war1.challenger_course_points)
   end
 end
