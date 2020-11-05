bc3a11e9d885ad3b3a74eec92c0528a895024825
Philip DeFraties <philipjfry338@gmail.coom>
2020-11-04 20:46:30 -0700

    Added pry to save spot in failing spec

--- app/controllers/api/v1/students/courses_controller.rb
+++ app/controllers/api/v1/students/courses_controller.rb
@@ -19,6 +19,7 @@ module Api
 
         def destroy
           result = CourseStudent.where('student_id = ? AND course_id = ?', course_params[:student_id], course_params[:course_id])
+          binding.pry
           result.destroy_all
         end
 
