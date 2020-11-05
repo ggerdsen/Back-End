c26adc1fad20fffe2ce635cd4107d305a000ce1e
Arique1104 <60521756+Arique1104@users.noreply.github.com>
2020-11-05 15:50:30 -0600

    Removes duplicate schema migrations

--- db/migrate/20201029233934_change_student_name_attribute.rb
+++
@@ -1,7 +0,0 @@
-class ChangeStudentNameAttribute < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :students, :name
-    add_column :students, :first_name, :string
-    add_column :students, :last_name, :string
-  end
-end
