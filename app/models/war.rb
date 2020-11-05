502bd0495a4f6f7b91a54d58bae384fea80786a4
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-05 15:23:15 -0700

    Add opponent_course_name to war model
    Co-authored-by: Priya Power <49959312+priyapower@users.noreply.github.com>

--- app/models/war.rb
+++ app/models/war.rb
@@ -4,7 +4,8 @@ class War < ApplicationRecord
   validates_presence_of :challenger_course_id,
                         :opponent_course_id,
                         :challenger_course_points,
-                        :opponent_course_points
+                        :opponent_course_points,
+                        :opponent_course_name
 
   validates_associated :teacher
 end
