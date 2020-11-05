f93fd3142c8677d566d66586fad54cb7e4aec244
Saryn Mooney <smooney15@my.whitworth.edu>
2020-11-05 16:18:36 -0700

    add opponent course name to wars serializer

--- app/serializers/war_serializer.rb
+++ app/serializers/war_serializer.rb
@@ -1,4 +1,4 @@
 class WarSerializer < BaseSerializer
-  attributes :challenger_course_id, :opponent_course_id, :challenger_course_points, :opponent_course_points, :teacher_id
+  attributes :challenger_course_id, :opponent_course_id, :challenger_course_points, :opponent_course_points, :teacher_id, :opponent_course_name
   belongs_to :teacher
 end
